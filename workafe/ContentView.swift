acf2710a85743cb554ce96815840a2f18362c2b9
Hiroki11323 <basketball11dmdm@gmail.com>
2024-05-11 16:53:51 +0900

    SwiftUIによるプロジェクト作成

---
+++ workafe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  workafe
+//
+//  Created by 本前宏樹 on 2024/05/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ workafe/workafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  workafeApp.swift
+//  workafe
+//
+//  Created by 本前宏樹 on 2024/05/11.
+//
+
+import SwiftUI
+
+@main
+struct workafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
